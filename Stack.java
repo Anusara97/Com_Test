af77f40ffbc4ce62ec59c530d0cc61fa2f530445
Anusara97 <anusara777rcm@gmail.com>
2025-05-19 23:25:48 +0530

    push() method is created in B1.

--- Stack.java
+++ Stack.java
@@ -14,4 +14,12 @@ public class Stack {
     public boolean isFull() {
         return top == stk.length - 1;
     }
+
+    public void push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is overflow.");
+        } else {
+            stk[++top] = item;
+        }
+    }
 }
